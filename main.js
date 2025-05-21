a74b88d2b92ee21be27f2b5f2fc798468f70eb1c
muhaguzan <mgustifauzan2794@gmail.com>
2025-05-21 20:05:36 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function clicks() {
+    $('#div1').hide();
+    $('#div2').show();
+    $('#div2').addClass("fadeinv2");
+        
+    var typed = new Typed('.katakata', {
+        strings: ["Cuma mau bilang", "Selamat Malam feyyy", "Jangan menyerah yaa sama dunia", "Saat lelah menghampiri, beristirahatlah sejenak, tetapi jangan pernah berhenti dan menyerah.","Stayy strong brooooo"],
+        typeSpeed: 70
+    });
+}
